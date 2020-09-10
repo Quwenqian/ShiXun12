476b5e82ca077206cc560a009b7e4cee8d156f03
Quwenqian <qu.wenqian@icloud.com>
2020-09-10 10:43:11 +0800

    add erueka server project to github

---
+++ bug_eureka_server/src/main/java/com/dfbz/bugSystem/BugEurekaServerApplication.java
@@ -0,0 +1,15 @@
+package com.dfbz.bugSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class BugEurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BugEurekaServerApplication.class, args);
+    }
+
+}
